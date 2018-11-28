8305450856c38e6c49c33bee8769293a03d11ea2
Bhagya B Raj <44748965+BhagyaBaburajan@users.noreply.github.com>
2018-11-28 19:44:14 +0530

    circle drawing

---
+++ Cirlce.java
@@ -0,0 +1,18 @@
+package pack;
+public class Circle
+{
+	double r=5;
+	public void area()
+	{
+		double a;
+		a=3.14*r*r;
+		System.out.println("Area is"+a);
+	}
+	public void circumference()
+	{
+		double c;
+		c=2*3.14*r;
+		System.out.println("circumference is"+c);
+	}
+}
+
